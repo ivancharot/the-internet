bf4d7659222eb640f89aaadcac2db32de1cc3f59
ivancharot <ivanCharot@gmail.com>
2023-04-18 20:20:37 +0300

    Add/Remove Elements page test

---
+++ test/pageobjects/add_remove.page.ts
@@ -0,0 +1,41 @@
+
+
+import { error } from 'console'
+import Page from './page'
+
+
+class AddRemovePage extends Page {
+
+    get addButton () {
+        return $('.example>button')
+    }
+
+    get addedElementsContainer () {
+        return $('#elements')
+    }
+
+    get addedElements () {
+        return this.addedElementsContainer.$$('.added-manually')
+    }
+
+    async addElements (howMany: number = 1) {
+        while (await this.addedElements.length!=howMany) {
+            await this.addButton.click()
+        }
+        return this
+    }
+
+    async deleteElements (howMany: number = 1) {
+        const elementsToStay = await this.addedElements.length-howMany
+        while (await this.addedElements.length!=elementsToStay) {
+            await this.addedElements[0].click()
+        }   
+        return this
+    }
+
+    async open() {
+        return super.open('add_remove_elements/')
+    }
+}
+
+export default new AddRemovePage()
--- test/specs/example.e2e.ts
+++ test/specs/example.e2e.ts
@@ -1,14 +1,34 @@
 import LoginPage from '../pageobjects/login.page'
 import SecurePage from '../pageobjects/secure.page'
+import AddRemovePage from '../pageobjects/add_remove.page'
 
 describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
+    it('should login with valid credentials', async function() {
         await LoginPage.open()
 
         await LoginPage.login('tomsmith', 'SuperSecretPassword!')
         await expect(SecurePage.flashAlert).toBeExisting()
         await expect(SecurePage.flashAlert).toHaveTextContaining(
             'You logged into a secure area!')
+    })
+})
+
+describe.only('Add/Remove Elements page', () => {
+    //times to click on button
+    const amountOfElements = 4
+
+    beforeEach(async function() {
+        await AddRemovePage.open()
+        await AddRemovePage.addElements(amountOfElements)
+    })
+
+    it('button ADD should add elements with exact amount', async function() {
+        await expect(AddRemovePage.addedElementsContainer).toHaveChildren(amountOfElements)
+    })
+
+    it.only('click on added elements to delete them', async function () {
+        await AddRemovePage.deleteElements(amountOfElements)
+        await expect(AddRemovePage.addedElementsContainer).toHaveChildren(0)
     })
 })
 
