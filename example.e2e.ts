4d95c41a19690a5abe3e28a884e8ee1618360514
ivancharot <ivanCharot@gmail.com>
2023-04-20 13:51:15 +0300

    fixed imports with index.ts

--- example.e2e.ts
+++ example.e2e.ts
@@ -1,7 +1,7 @@
 import { LoginPage, 
     SecurePage, 
     AddRemovePage, 
-    BasicAuthPage } from '../../imports'
+    BasicAuthPage } from './test/pageobjects'
 
 
 describe('My Login application', () => {
@@ -9,7 +9,6 @@ describe('My Login application', () => {
     const securePage = new SecurePage()
     it('should login with valid credentials', async function() {
         await loginPage.open()
-
         await loginPage.login('tomsmith', 'SuperSecretPassword!')
         await expect(securePage.flashAlert).toBeExisting()
         await expect(securePage.flashAlert).toHaveTextContaining(
---
+++ test/pageobjects/index.ts
@@ -0,0 +1,4 @@
+export * from './add_remove.page'
+export * from './basic_auth.page'
+export * from './login.page'
+export * from './secure.page'
